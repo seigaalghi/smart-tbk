39ef1d425f05b1cb5e25c64273901554a9302de9
seigaalghi <seigaalghi@gmail.com>
2025-01-02 22:35:02 +0700

    add submission

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  function handleInput() {
-    //please put your logic here
+  const [number, setNumber] = useState("");
+  const [result, setResult] = useState(0);
+
+  function handleInput(e) {
+    const value = e.target.value;
+    if (value == "") {
+      setNumber(value)
+      return
+    }
+    if (!isNaN(parseInt(value))) {
+      setNumber(parseInt(value, 10).toString());
+    }
+  }
+
+  function handleSubmit() {
+    if (number === "") return;
+    const reversedNumber = parseInt(number.split("").reverse().join(""), 10);
+    const difference = Math.abs(parseInt(number, 10) - reversedNumber);
+    setResult(difference);
   }
 
   return (
     <div className="App">
       <div>
-        Number: <input />
-        <button>Submit</button>
+        Number: <input value={number} onChange={handleInput} />
+        <button onClick={handleSubmit}>Submit</button>
       </div>
-      <div>Result: 0</div>
+      <div>Result: {result}</div>
     </div>
   );
 }
